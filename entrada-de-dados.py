b71195ae70bce25baa77ae2011f4b4627a7136de
Lucas de Paiva Ribeiro <lucas@flaire.local>
2022-10-25 12:22:39 -0300

    Dando exemplo de entrada de dados com definição do tipo de variável antes e depois.

--- entrada-de-dados.py
+++ entrada-de-dados.py
@@ -1,10 +1,21 @@
 """
 Entrada de dados: input
 """
-nome = input("Qual o seu nome? ")
-idade = input("Qual a sua idade? ")
-ano_atual = int(input("Em que ano estamos..? "))
+nome_exemplo = input("Qual o seu nome? ")
+idade_exemplo = input("Qual a sua idade? ")
+ano_atual_exemplo = input("Em que ano estamos..? ")
 
-data_nasc = ano_atual - int(idade)
+data_nasc = (int(ano_atual_exemplo) - int(idade_exemplo))
 
-print(f"{nome} tem {idade} anos e seu ano de nascimento é de {data_nasc}")
+print(f"{nome_exemplo} tem {idade_exemplo} anos e seu ano de nascimento é de {data_nasc}")
+
+
+# definir tipo de variável junto com input
+
+nome_exemplo2 = str(input("Qual o seu nome? "))
+idade_exemplo2 = int(input("Qual sua idade? "))
+ano_atual_exemplo2 = int(input("Em que ano estamos..? "))
+
+data_nasc2 = (ano_atual_exemplo2 - idade_exemplo2)
+
+print(f"{nome_exemplo2} tem {idade_exemplo2} anos e seu ano de nascimento é de {data_nasc2}")
