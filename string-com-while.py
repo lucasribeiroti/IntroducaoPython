a2983bf1b3e9129cb3a15a5bbf9125be9508dd3c
Lucas de Paiva Ribeiro <lucas@flaire.local>
2022-10-26 15:28:37 -0300

    Correção do string-com-while, estav acom erro de indexação.

--- string-com-while.py
+++ string-com-while.py
@@ -1,11 +1,11 @@
 """
 Iterando strings com while
 """
-frase = "Se não tem a manha não entra não"
+frase = "Se não tem a manha não entra não"  # são 32 caracteres (do 0 ao 31).
 tamanho_frase = len(frase)
 contador = 0
 
-while contador < 33:
+while contador <= 31:  # por isso aqui é 31 e não 32, pois começa do 0 e vai até o 31.
     print(frase[contador], contador)
     contador += 1
 
